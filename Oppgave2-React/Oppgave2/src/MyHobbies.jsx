2df7dbc87fd2b05c2e4469818c1eed3772a68600
Path1an <odintobiassen@gmail.com>
2024-08-29 09:52:42 +0200

    added buttons and CSS

--- Oppgave2-React/Oppgave2/src/MyHobbies.jsx
+++ Oppgave2-React/Oppgave2/src/MyHobbies.jsx
@@ -1,24 +1,51 @@
+import {useState} from 'react'
+import './MyHobbies.css'
+
 const hobbies = [
-    {title: "utvikling", isFavore: true, upVote: 0, id: 1},
-    {title: "Gaming", isFavore: false, upVote: 0, id: 2},
+    {title: "utvikling", isFavore: false, upVote: 0, id: 1},
+    {title: "Gaming", isFavore: true, upVote: 0, id: 2},
     {title: "Scouting", isFavore: false, upVote: 0, id: 3},
-    {title: "Dungeons and Dragons", isFavore: false, upVote: 0, id: 4},
+    {title: "Dungeons and Dragons", isFavore: true, upVote: 0, id: 4},
 ]
 
 
 export default function MyHobbies() {
 
     const listItems = hobbies.map(hobby =>
-        <li key={hobby.id}>
+        <li key={hobby.id} 
+        style={
+            {color:hobby.isFavore ? 'magenta' : 'aqua'}
+        }>
+            
             {hobby.title}
+            <VoteButton></VoteButton>
         </li>
     )
 
     return(
         <>
-        <h1>Odin Tobiassen</h1>
+        <h1 className='header'>Odin Tobiassen</h1>
         <ul>{listItems}</ul>
         </>
         
+    )
+}
+
+
+
+function VoteButton() {
+    const[vote, setVote]= useState(0)
+    
+    function handleClick() {
+        setVote(vote +1)
+    }
+
+
+    return(
+
+        <button className='button' onClick={handleClick}>
+            voted {vote} times
+        </button>
+
     )
 }
