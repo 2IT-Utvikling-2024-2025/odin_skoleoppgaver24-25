d9feec70a80686d84d9baea796a2a9141f10ef4f
Path1an <odintobiassen@gmail.com>
2025-02-18 20:39:40 +0100

    updates routs and routRoutes

--- fullstack-applikasjon/RuterAPI/app.js
+++ fullstack-applikasjon/RuterAPI/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const routRoutes = require('./v1/routes/routRoutes');
+
 
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+
+//Define Routes
+app.use('/v1/routes', routRoutes);
+
 
 //Routes
 app.listen(3000, () => 
---
+++ fullstack-applikasjon/RuterAPI/v1/routes/routRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { getAllRouts, getRoutById, createRout, updateRout, deleteRout } = require('../controllers/routsController');
+
+const router = express.Router();
+
+//Define Routes
+router.get('/', getAllRouts);
+router.get('/:id', getRoutById);
+router.post('/', createRout);
+router.put('/:id', updateRout);
+router.delete('/:id', deleteRout);
+
+module.exports = router;
